ed5df93b7a4bb756927bc462dd82aed8f9469676
Szász Áron <szaszjozsefg9@gmail.com>
2022-10-16 18:22:46 +0200

    reversi alapok

--- MasodikF/reversi.js
+++ MasodikF/reversi.js
@@ -1,1 +1,18 @@
 'pragma once'
+// 0 = kék ; 1 = piros
+
+let tablaData = [];
+
+class Cella {
+
+    constructor(ertek, szin){
+        this.ertek = ertek;
+        this.szin = szin;
+    }
+}
+
+function jatekSetup(){
+    let meret = document.getElementById('tablaMeret').value;
+
+
+}
