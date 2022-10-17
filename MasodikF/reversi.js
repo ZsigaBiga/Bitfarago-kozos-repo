a1bd46b605a14d691482a3d984a21340739d64d0
Szász Áron <szaszjozsefg9@gmail.com>
2022-10-17 22:38:57 +0200

    Tábla és értékek kész

--- MasodikF/reversi.js
+++ MasodikF/reversi.js
@@ -1,7 +1,7 @@
 'pragma once'
-// 0 = kék ; 1 = piros
+// true = kék ; false = piros
 
-let tablaData = new Array(2);
+let tablaData = [];
 
 class Cella {
     constructor(ertek, szin){
@@ -15,8 +15,6 @@ function jatekSetup(){
     tablaHelye.innerHTML = "";
 
     let meret = document.getElementById('tablaMeret').value;
-    let sorok = Math.ceil(meret / 2);
-    let ertek = 1;
 
     let tabla = document.createElement('table');
     tabla.style.width = '100%';
@@ -24,41 +22,98 @@ function jatekSetup(){
 
     let tablaTest = document.createElement('tbody');
 
-    var elol = 0;
-    var hatul = meret - 1;
+    let jelenlegi = 1;
+    let voltE = false;
+    let kesz = false;
 
-    while (ertek != sorok + 1) {
-        
-        for (let i = elol; i <= hatul; i++) {
-            let sor = document.createElement('tr');
-            tablaData[i] = new Array(2);
-            sor.style.width = '100%';
-            
-            console.log(i);
-            for (let j = elol; j <= hatul; j++) {
-                
-                if(i != elol || i != hatul && j != elol || j!= hatul){
-                    
-                    var cella = new Cella(ertek, false);
-                    tablaData[i][j] = cella;   
-                    
-                    let mezo = document.createElement('td');
-                    mezo.style.textAlign = 'center';
-                    mezo.style.width = 500 / meret + 'px'; 
-                    mezo.style.height = 500 / meret + 'px';
-                    mezo.innerHTML = tablaData[i][j].ertek;
-                    sor.appendChild(mezo);            
-        
-                }
-                console.log(j);
-            }
-            
-            tablaTest.appendChild(sor);
+    for(let i = 0; i < meret; i++)
+    {
+        tablaData[i] = new Array(2);
+        let oszlop = 0;
+    
+        for(let k = 1; k <= jelenlegi; k++)
+        {
+            var cella = new Cella(k, null);
+            tablaData[i][oszlop] = cella;
+
+            oszlop++;
         }
-        elol++;
-        hatul--;
-        ++ertek;
+
+        for(let h = jelenlegi; h <= meret - jelenlegi; h++)
+        {
+            var cella = new Cella(jelenlegi, null);
+            tablaData[i][oszlop] = cella;
+
+            oszlop++;
+        }    
+        
+        for(let l = jelenlegi - 1; l > 0; l--)
+        {
+            var cella = new Cella(l, null);
+            tablaData[i][oszlop] = cella;
+
+            oszlop++;
+        }
+        
+
+        if(jelenlegi + 1 <= Math.round(meret/2) && kesz == false)
+        {
+            jelenlegi++;
+            
+        }
+        else if(jelenlegi == Math.round(meret/2))
+        {
+
+            if(voltE == false)
+            {
+                voltE = true;
+            }
+            else
+            {
+                kesz = true;
+                jelenlegi--;
+            }           
+        }
+        else
+        {
+            jelenlegi--;
+        }
     }
+    var kozep = meret / 2 - 1;
+    tablaData[kozep][kozep].szin = false; 
+    tablaData[kozep][kozep + 1].szin = true;
+    tablaData[kozep + 1][kozep].szin = true;
+    tablaData[kozep + 1][kozep + 1].szin = false;
+
+    for (let i = 0; i < meret; i++) {
+        let sor = document.createElement('tr');
+        sor.style.width = '100%';
+
+        for (let j = 0; j < meret; j++) {
+            
+            let mezo = document.createElement('td');
+            mezo.style.textAlign = 'center';
+            mezo.style.width = 500 / meret + 'px'; 
+            mezo.style.height = 500 / meret + 'px';
+            mezo.style.border = '1px solid black';
+            
+            if (tablaData[i][j].szin) {
+                let korong = document.createElement('img');
+                korong.src = "./kepek/kekkorong.png";
+                mezo.appendChild(korong);
+            }
+            else if(tablaData[i][j].szin == false){
+                let korong = document.createElement('img');
+                korong.src = "./kepek/piroskorong.png";
+                mezo.appendChild(korong);
+            }
+
+            sor.appendChild(mezo);            
+        }
+        
+        tablaTest.appendChild(sor);
+    }
+
     tabla.appendChild(tablaTest);
     tablaHelye.appendChild(tabla);
 }
