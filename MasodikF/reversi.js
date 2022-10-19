efc9ac3c06af86789a1c96a4ea393a68430fb436
Szász Áron <szaszjozsefg9@gmail.com>
2022-10-19 03:17:49 +0200

    Majdnem kész de rossz irányok

--- MasodikF/reversi.js
+++ MasodikF/reversi.js
@@ -1,22 +1,87 @@
 'pragma once'
+
 // true = kék ; false = piros
 
 let tablaData = [];
+
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
+let displayseconds = 0;
+let displayminutes = 0;
+let displayhours = 0;
+
+let loop;
+
+let meret, tabla, tablaHelye;
 
 class Cella {
     constructor(ertek, szin){
         this.ertek = ertek;
         this.szin = szin;
+        this.kattint = false;
     }
 }
 
+function stopper(){
+    
+        seconds++;
+    
+        if(seconds / 60 === 1){
+            seconds = 0;
+            minutes++;
+    
+            if(minutes / 60 === 1){
+                minutes = 0;
+                hours++;
+            }
+        }
+    
+        if(seconds < 10){
+            displayseconds = "0" + seconds.toString();
+        }
+        else{
+            displayseconds = seconds;
+        }
+    
+        if(minutes < 10){
+            displayminutes = "0" + minutes.toString();
+        }
+        else{
+            displayminutes = minutes;
+        }
+    
+        if(hours < 10){
+            displayhours = "0" + hours.toString();
+        }
+        else{
+            displayhours = hours;
+        }
+    
+        document.getElementById("display").innerHTML = displayhours + ":" + displayminutes + ":" + displayseconds;
+        
+}
+
 function jatekSetup(){
-    let tablaHelye = document.getElementById('jatekHely');
-    tablaHelye.innerHTML = "";
 
-    let meret = document.getElementById('tablaMeret').value;
+    if(loop){
+        seconds = 0;
+        minutes = 0;
+        hours = 0;
 
-    let tabla = document.createElement('table');
+        displayseconds = 0;
+        displayminutes = 0;
+        displayhours = 0;
+        clearInterval(loop);
+    }
+
+    tablaHelye = document.getElementById('jatekHely');
+
+    meret = document.getElementById('tablaMeret').value;
+
+    tabla = document.getElementById('jatekTabla');
+    tabla.innerHTML = "";
     tabla.style.width = '100%';
     tabla.setAttribute('border', '1');
 
@@ -82,8 +147,13 @@ function jatekSetup(){
     var kozep = meret / 2 - 1;
     tablaData[kozep][kozep].szin = false; 
     tablaData[kozep][kozep + 1].szin = true;
+    tablaData[kozep - 2][kozep + 1].szin = true;
+    tablaData[kozep - 1][kozep + 1].szin = false;
+    tablaData[kozep - 1][kozep + 2].szin = false;
     tablaData[kozep + 1][kozep].szin = true;
     tablaData[kozep + 1][kozep + 1].szin = false;
+    tablaData[kozep + 2][kozep + 1].szin = false;
+
 
     for (let i = 0; i < meret; i++) {
         let sor = document.createElement('tr');
@@ -92,6 +162,7 @@ function jatekSetup(){
         for (let j = 0; j < meret; j++) {
             
             let mezo = document.createElement('td');
+            mezo.setAttribute('id', ""+i+j)
             mezo.style.textAlign = 'center';
             mezo.style.width = 500 / meret + 'px'; 
             mezo.style.height = 500 / meret + 'px';
@@ -116,4 +187,249 @@ function jatekSetup(){
 
     tabla.appendChild(tablaTest);
     tablaHelye.appendChild(tabla);
+    loop = setInterval(() => {
+        stopper();
+    }, 1000);
+
+    kor(false);
+}
+
+function kor(beSzin){
+
+    for (let i = 0; i < meret; i++) {
+        
+        for(let j = 0; j < meret; j++){
+            
+            if(tablaData[i][j].szin == beSzin){
+                szomszedCheck(tablaData[i][j], i, j);                
+            }
+        }    
+    }
+
+
+}
+
+function szomszedCheck(cella, bei, bej){
+
+    //Elnézést kérek az elkövetkező, kínzásnak felírható, embertelen, fájdalmas kódrészletért. Kitartást.
+
+    for(let i = 0; i < 4; i++){
+        let idx1;
+        let idx2; 
+        
+        let masSzinVolt1 = false;
+        let masSzinVolt2 = false;
+
+        switch(i){
+
+            //Fel-Le
+            case 0:
+                idx1 = bei - 1;
+                idx2 = bei + 1;
+
+                while(idx1 != -1 || idx2 != meret){
+
+                    if (idx1 != -1) {
+                        
+                        if(tablaData[idx1][bej].szin == !cella.szin){
+                            idx1--;
+                            masSzinVolt1 = true;
+                        }
+                        else if(tablaData[idx1][bej].szin == null && masSzinVolt1){
+                            
+                            tablaData[idx1][bej].kattint = true;
+                            var cell = document.getElementById(''+idx1+bej);
+                            cell.style.backgroundColor = "red";
+                            idx1 = -1;
+                        }
+                        else if((tablaData[idx1][bej].szin == cella.szin || tablaData[idx1][bej].szin == null ) && !masSzinVolt1 || (tablaData[idx1][bej].szin == cella.szin || tablaData[idx1][bej].szin == null ) && masSzinVolt1){
+                            idx1 = -1;
+                        }
+                        
+                    }
+
+                    if (idx2 != meret) {
+                        
+                        if(tablaData[idx2][bej].szin == !cella.szin){
+                            idx2++;
+                            masSzinVolt2 = true;
+                        }
+                        else if(tablaData[idx2][bej].szin == null && masSzinVolt2){
+                            
+                            tablaData[idx2][bej].kattint = true;
+                            var cell = document.getElementById(''+idx2+bej);
+                            cell.style.backgroundColor = "red";
+                            idx2 = meret;
+                        }
+                        else if((tablaData[idx2][bej].szin == cella.szin || tablaData[idx2][bej].szin == null) && !masSzinVolt2 || (tablaData[idx2][bej].szin == cella.szin || tablaData[idx2][bej].szin == null ) && masSzinVolt2){
+                            idx2 = meret;
+                        }
+                    }
+
+                }
+                break;
+
+                //Balra-Jobbra
+                case 1:
+                idx1 = bej - 1;
+                idx2 = bej + 1;
+
+                masSzinVolt1 = false;
+                masSzinVolt2 = false;
+
+                veszelyszok = 0;
+
+                while(idx1 != -1 || idx2 != meret){
+
+                    if (idx1 != -1) {
+                        
+                        if(tablaData[bei][idx1].szin == !cella.szin){
+                            idx1--;
+                            masSzinVolt1 = true;
+                        }
+                        else if(tablaData[bei][idx1].szin == null && masSzinVolt1){
+                            
+                            tablaData[bei][idx1].kattint = true;
+                            var cell = document.getElementById(''+bei+idx1);
+                            cell.style.backgroundColor = "red";
+                            idx1 = -1;
+                        }
+                        else if((tablaData[bei][idx1].szin == cella.szin || tablaData[bei][idx1].szin == null ) && !masSzinVolt1 || (tablaData[bei][idx1].szin == cella.szin || tablaData[bei][idx1].szin == null ) && masSzinVolt1){
+                            idx1 = -1;
+                        }
+                        
+                    }
+
+                    if (idx2 != meret) {
+                        
+                        if(tablaData[bei][idx2].szin == !cella.szin){
+                            idx2++;
+                            masSzinVolt2 = true;
+                        }
+                        else if(tablaData[bei][idx2].szin == null && masSzinVolt2){
+                            
+                            tablaData[bei][idx2].kattint = true;
+                            var cell = document.getElementById(''+bei+idx2);
+                            cell.style.backgroundColor = "red";
+                            idx2 = meret;
+                        }
+                        else if((tablaData[bei][idx2].szin == cella.szin || tablaData[bei][idx2].szin == null) && !masSzinVolt2 || (tablaData[bei][idx2].szin == cella.szin || tablaData[bei][idx2].szin == null ) && masSzinVolt2){
+                            idx2 = meret;
+                        }
+                    }
+
+                    veszelyszok++;
+
+                    if(veszelyszok > 9){
+                        console.log('hulebuzta');
+                        console.log(idx1);
+                        break;
+                    }
+                }
+                break;
+
+                //Srég-balradőlt
+                case 2:
+                idx1 = Number(bei);
+                idx2 = Number(bei + 1);
+
+                masSzinVolt1 = false;
+                masSzinVolt2 = false;
+
+                while(idx1 != -1 || idx2 != meret){
+
+                    if (idx1 != -1) {
+                        
+                        
+
+                        if(tablaData[idx1][idx1 - 1].szin == !cella.szin){
+                            idx1--;
+                            masSzinVolt1 = true;
+                        }
+                        else if(tablaData[idx1][idx1 - 1].szin == null && masSzinVolt1){
+                            
+                            tablaData[idx1][idx1 - 1].kattint = true;
+                            var cell = document.getElementById(''+idx1+''+ (idx1 - 1));
+                            cell.style.backgroundColor = "red";
+                            idx1 = -1;
+                        }
+                        else if((tablaData[idx1][idx1 - 1].szin == cella.szin || tablaData[idx1][idx1 - 1].szin == null ) && !masSzinVolt1 || (tablaData[idx1][idx1 - 1].szin == cella.szin || tablaData[idx1][idx1 - 1].szin == null ) && masSzinVolt1){
+                            idx1 = -1;
+                        }
+                        
+                    }
+                    
+
+                    if (idx2 != meret) {
+                        if(tablaData[idx2][idx2].szin == !cella.szin){
+                            idx2++;
+                            masSzinVolt2 = true;
+                        }
+                        else if(tablaData[idx2][idx2].szin == null && masSzinVolt2){
+                            
+                            tablaData[idx2][idx2].kattint = true;
+                            var cell = document.getElementById(''+idx2+''+ idx2++);
+                            cell.style.backgroundColor = "red";
+                            cell.innerHTML += 'a';
+                            idx2 = meret;
+                        }
+                        else if((tablaData[idx2][idx2].szin == cella.szin || tablaData[idx2][idx2].szin == null) && !masSzinVolt2 || (tablaData[idx2][idx2].szin == cella.szin || tablaData[idx2][idx2].szin == null ) && masSzinVolt2){
+                            idx2 = meret;
+                        }
+                    }
+                }
+                break;
+
+                //Srég-jobbradőlt
+                case 3:
+                idx1 = Number(bei - 1);
+                idx2 = Number(bei + 1);
+
+                masSzinVolt1 = false;
+                masSzinVolt2 = false;
+
+                while(idx1 != -1 || idx2 != meret){
+
+                    if (idx1 != -1) {
+                        
+                        if(tablaData[idx1][idx1 + 1].szin == !cella.szin){
+                            idx1--;
+                            masSzinVolt1 = true;
+                        }
+                        else if(tablaData[idx1][idx1 + 1].szin == null && masSzinVolt1){
+                            
+                            tablaData[idx1][idx1 + 1].kattint = true;
+                            var cell = document.getElementById(''+idx1+''+ (idx1));
+                            cell.style.backgroundColor = "red";
+                            idx1 = -1;
+                        }
+                        else if((tablaData[idx1][idx1 + 1].szin == cella.szin || tablaData[idx1][idx1 + 1].szin == null ) && !masSzinVolt1 || (tablaData[idx1][idx1 + 1].szin == cella.szin || tablaData[idx1][idx1 + 1].szin == null ) && masSzinVolt1){
+                            idx1 = -1;
+                        }
+                        
+                    }
+                    
+                    
+                    if (idx2 != meret) {
+                        if(tablaData[idx2][idx2 - 1].szin == !cella.szin){
+                            idx2++;
+                            masSzinVolt2 = true;
+                        }
+                        else if(tablaData[idx2][idx2 - 1].szin == null && masSzinVolt2){
+                            
+                            var cell = document.getElementById(''+idx2+''+ (idx2 - 1));
+                            cell.style.backgroundColor = "green";
+                            tablaData[idx2][idx2 - 1].kattint = true;
+                            cell.innerHTML += 'a';
+                            idx2 = meret;
+                        }
+                        else if((tablaData[idx2][idx2 - 1].szin == cella.szin || tablaData[idx2][idx2 - 1].szin == null) && !masSzinVolt2 || (tablaData[idx2][idx2 - 1].szin == cella.szin || tablaData[idx2][idx2 - 1].szin == null ) && masSzinVolt2){
+                            idx2 = meret;
+                        }
+                    }
+                }
+                break;
+        }
+    }
+
 }
