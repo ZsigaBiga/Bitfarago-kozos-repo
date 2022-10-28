325a2471057dbb56346904b4d1e2e30f08af40e9
Szász Áron <szaszjozsefg9@gmail.com>
2022-10-29 00:32:10 +0200

    Kész, de hibás collision detection

--- HarmadikF/script.js
+++ HarmadikF/script.js
@@ -47,7 +47,7 @@ function player(xHely, yHely)  {
         ctx.beginPath();
         ctx.fillStyle = "black";
         ctx.fillRect(this.x, this.y, this.meret, this.meret);
-        ctx.fill();
+        ctx.closePath();
     }
 
     this.newPos = function(){
@@ -68,21 +68,99 @@ function player(xHely, yHely)  {
         var utkoz = true;
 
         if((jatekosAlso < falFelso) || (jatekosFelso > falAlso) || (jatekosJobb < falBal) || (jatekosBal > falJobb)){
-            utkoz = false;
-            
-        }
-        
-        if(utkoz){
+            utkoz = false;   
         }
 
         return utkoz;
     }
 }
 
+function hangSzenzor(xHely, yHely) {
+    this.x = xHely;
+    this.y = yHely;
+    this.radius = 0;
+    this.korM = 15;
+
+    this.erzekel = function (player) {
+        this.radius = player.spdSkala * 10 * 5;
+        var erzek = false;
+
+        if(Math.sqrt(Math.pow(this.x - (player.x + player.meret / 2), 2) + Math.pow(this.y - (player.y + player.meret / 2), 2)) <= this.radius + 100){
+            erzek = true;
+        }
+
+        return erzek;
+    }
+
+    this.update = function (trigger){
+        ctx = gameArea.context;
+
+        ctx.beginPath();
+        ctx.fillStyle = "red";
+
+        if(trigger){
+            ctx.fillStyle = "green";
+            ctx.arc(this.x, this.y, this.korM, 0, 2*Math.PI);
+        }else{
+            ctx.arc(this.x, this.y, this.korM, 0, 2*Math.PI);
+        }
+
+        ctx.fill();
+        ctx.fillStyle = "black";
+        
+        ctx.strokeStyle = "#ff990053";
+        ctx.moveTo(this.x + this.radius + 100, this.y);
+        ctx.arc(this.x, this.y, this.radius + 100, 0, 2*Math.PI);
+        ctx.stroke();
+    }
+}
+
+function mozgasSzenzor(xHely, yHely, inHossz, inMagas){
+    this.x = xHely;
+    this.y = yHely;
+    this.hossz = inHossz;
+    this.magas = inMagas;
+
+    this.erzekel = function(player){
+        var jatekosBal = player.x;
+        var jatekosJobb = player.x + player.meret;
+        var jatekosFelso = player.y;
+        var jatekosAlso = player.y + player.meret;
+        var szenzorBal = this.x;
+        var szenzorJobb = this.x + this.hossz;
+        var szenzorFelso = this.y;
+        var szenzorAlso = this.y + this.magas;
+
+        var utkoz = true;
+
+        if(((jatekosAlso < szenzorFelso) || (jatekosFelso > szenzorAlso) || (jatekosJobb < szenzorBal) || (jatekosBal > szenzorJobb)) || (player.xSpeed == 0 && player.ySpeed == 0)){
+            utkoz = false;   
+        }
+
+        return utkoz;
+    }
+
+    this.update = function(trigger){
+        ctx = gameArea.context;
+        
+        ctx.beginPath();
+        ctx.fillStyle = "#ff000053";
+        if(trigger){
+            ctx.fillStyle = "green";
+            ctx.fillRect(this.x, this.y, this.hossz, this.magas);
+        }else{
+            ctx.fillRect(this.x, this.y, this.hossz, this.magas);
+        }
+
+        ctx.fill();
+        ctx.fillStyle = "black";
+    }
+}
+
 var haz = {
     oldalak: [],
     epit: function(){
-        for(let i = 0; i < 10; i++){
+        for(let i = 0; i < 12; i++){
             this.oldalak[i] = {};
         }
 
@@ -130,6 +208,25 @@ var haz = {
         this.oldalak[7].x = 445;
         this.oldalak[7].y = 145;
 
+        this.oldalak[8].width = 150;
+        this.oldalak[8].height = 5;
+        this.oldalak[8].x = 145;
+        this.oldalak[8].y = 300;
+
+        this.oldalak[9].width = 5;
+        this.oldalak[9].height = 100;
+        this.oldalak[9].x = 295;
+        this.oldalak[9].y = 145;
+
+        this.oldalak[10].width = 150;
+        this.oldalak[10].height = 5;
+        this.oldalak[10].x = 145;
+        this.oldalak[10].y = 450;
+
+        this.oldalak[11].width = 5;
+        this.oldalak[11].height = 100;
+        this.oldalak[11].x = 295;
+        this.oldalak[11].y = 300;
     },
     draw: function () {
         ctx = gameArea.context;
@@ -138,7 +235,7 @@ var haz = {
         ctx.strokeStyle = "brown";
         ctx.lineWidth = 5;
 
-        for (let i = 0; i < 8; i++) {
+        for (let i = 0; i < 12; i++) {
             ctx.fillRect(this.oldalak[i].x, this.oldalak[i].y, this.oldalak[i].width, this.oldalak[i].height);
         }
          
@@ -149,6 +246,8 @@ var haz = {
 var wWidth;
 var wHeight;
 var juan;
+var hangSzenzorok = [];
+var mozgasSzenzorok = [];
 
 function updateGameArea(){
     
@@ -162,15 +261,13 @@ function updateGameArea(){
     if(gameArea.keys && gameArea.keys[119]) {juan.ySpeed = -1;}
     if(gameArea.keys && gameArea.keys[115]) {juan.ySpeed = 1;}
 
-    for (let i = 0; i < 8; i++) {
-        if(juan.collision(haz.oldalak[i])){
-            juan.x += juan.xSpeed * -1 * 4;
-            juan.y += juan.ySpeed * -1 * 4;
+    for (let i = 0; i < 12; i++) {
 
-            gameArea.context.beginPath();
-            gameArea.context.arc(haz.oldalak[i].x, haz.oldalak[i].y, 30, 0, 2*Math.Pi);
-            console.log("Üti");
-            gameArea.context.stroke();
+        // Nem jövök rá hogy kell odaclampelni dx
+
+        if(juan.collision(haz.oldalak[i])){
+            juan.x = juan.x + juan.xSpeed * -1 * 5;
+            juan.y = juan.y + juan.ySpeed * -1 * 5;
         }
     }
 
@@ -179,6 +276,14 @@ function updateGameArea(){
     if(gameArea.gorgo > 0 && (juan.spdSkala + 0.5 < 2.5)) {juan.spdSkala += 0.5;}
     if(gameArea.gorgo < 0 && (juan.spdSkala - 0.5 > 0.5)) {juan.spdSkala -= 0.5;}
     gameArea.gorgo = 0;
+
+    for (let i = 0; i < hangSzenzorok.length; i++) {
+        hangSzenzorok[i].update(hangSzenzorok[i].erzekel(juan));
+    }
+
+    for (let i = 0; i < mozgasSzenzorok.length; i++) {
+        mozgasSzenzorok[i].update(mozgasSzenzorok[i].erzekel(juan));
+    }
 
     haz.draw();
     juan.newPos();
@@ -192,4 +297,6 @@ function Game(){
     wHeight = gameArea.canvas.height;
     haz.epit();
     juan = new player(wWidth / 2 - 30, wHeight / 2 - 30, 1);
+    hangSzenzorok = [new hangSzenzor(420, 520), new hangSzenzor(270, 210), new hangSzenzor(560, 370), new hangSzenzor(270, 560)];
+    mozgasSzenzorok = [new mozgasSzenzor(642, 570, 5, 77), new mozgasSzenzor(295, 245, 5, 70), new mozgasSzenzor(445, 220, 5, 70), new mozgasSzenzor(445, 385, 5, 70), new mozgasSzenzor(295, 385, 5, 70)];
 }
